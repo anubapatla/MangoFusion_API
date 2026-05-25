853c8ec2c2883b529ad52801d79fbe2299b3e3d3
AnuBapatla <anusrichatala@gmail.com>
2026-05-25 16:23:17 -0400

    Api Response

---
+++ Models/ApiResponse.cs
@@ -0,0 +1,13 @@
+﻿using System.Net;
+
+namespace MangoFusion_API.Models
+{
+    public class ApiResponse
+    {
+        public HttpStatusCode StatusCode { get; set; }
+        public bool IsSuccess { get; set; } = true;
+
+        public List<string> ErrorMessage { get; set; } = [];
+        public object ? Result { get; set; }
+    }
+}
