273cf9dfefd9e651d99c4137148cbb7ef590456c
AnuBapatla <anusrichatala@gmail.com>
2026-05-27 14:05:21 -0400

    DTOs update part2

--- Models/Dto/MenuItemUpdateDTO.cs
+++ Models/Dto/MenuItemUpdateDTO.cs
@@ -6,14 +6,15 @@ namespace MangoFusion_API.Models.Dto
     {
         [Key]
         public int Id { get; set; }
+        [Required]
         public string Name { get; set; } = string.Empty;
-        public string Description { get; set; } = string.Empty;
+        public string? Description { get; set; } 
         public string Category { get; set; } = string.Empty;
 
-        public string SpecialTag { get; set; } = string.Empty;
+        public string? SpecialTag { get; set; } 
         [Range(1, 1000)]
         public double Price { get; set; }
-        [Required]
-        public IFormFile File { get; set; } = null!;
+        
+        public IFormFile? File { get; set; } = null!;
     }
 }
