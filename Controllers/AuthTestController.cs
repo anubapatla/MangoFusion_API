e0ae4f6f9b216a643c1225399249d2b52ac3f1bb
AnuBapatla <anusrichatala@gmail.com>
2026-05-30 22:36:14 -0400

    created AuthTest controller

---
+++ Controllers/AuthTestController.cs
@@ -0,0 +1,24 @@
+﻿using MangoFusion_API.Utility;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MangoFusion_API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthTestController : Controller
+    {
+        [HttpGet]
+        [Authorize]
+        public ActionResult<string> GetSomething()
+        {
+            return "You are authorized to see this message.";
+        }
+        [HttpGet("{someValue:int}")]
+        [Authorize(Roles =SD.Role_Admin)]
+        public ActionResult<string> GetSomething( int someValue)
+        {
+            return "You are authorized to see this message with role of admin";
+        }
+    }
+}
