8722f3ada4bb16bf983436c10045879e94fe2e94
AnuBapatla <anusrichatala@gmail.com>
2026-06-10 13:34:36 -0400

    Add rating field

---
+++ Models/Dto/OrderDetailsUpdateDTO.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MangoFusion_API.Models.Dto
+{
+    public class OrderDetailsUpdateDTO
+    {
+        [Required]
+        public int OrderDetailId { get; set; }
+
+        [Required]
+        public int MenuItemId { get; set; }
+
+        [Required]
+        public int Rating { get; set; }
+        
+    }
+}
