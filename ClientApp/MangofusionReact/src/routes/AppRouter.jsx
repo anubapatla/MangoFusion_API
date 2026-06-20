96f4577515038a9760d3a3710012927702c1f85a
AnuBapatla <anusrichatala@gmail.com>
2026-06-19 22:28:22 -0400

    constants for routes

--- ClientApp/MangofusionReact/src/routes/AppRouter.jsx
+++ ClientApp/MangofusionReact/src/routes/AppRouter.jsx
@@ -1,16 +1,24 @@
 import {  Routes, Route } from "react-router-dom";
 import Home from "../pages/Home";
+import Login from "../pages/auth/Login";
+import Register from "../pages/auth/Register";
+import Cart from "../pages/cart/Cart";
+import Checkout from "../pages/cart/Checkout";
+import OrderConfirmation from "../pages/order/OrderConfirmation";
+import MenuitemManagement from "../pages/menu/MenuitemManagement";
+import OrderManagement from "../pages/order/OrderManagement";
+import { ROUTES } from "../utility/constants";
 const Approutes =()=>{
     return (
     <Routes>
-        <Route path="/" element={<Home/>}/>
-        {/* <Route path="/login" element={<Login/>}/> 
-        <Route path="/register" element={<Register/>}/>
-        <Route path="/cart" element={<Cart/>}/>
-        <Route path="/checkout" element={<Checkout/>}/>
-        <Route path="/order-confirmation" element={<OrderConfirmation/>}/>
-        <Route path="/menu-management" element={<MenuitemManagement/>}/>
-        <Route path="/order-management" element={<OrderManagement/>}/>*/}
+        <Route path={ROUTES.HOME} element={<Home/>}/>
+        <Route path={ROUTES.LOGIN} element={<Login/>}/> 
+        <Route path={ROUTES.REGISTER} element={<Register/>}/>
+        <Route path={ROUTES.CART} element={<Cart/>}/>
+        <Route path={ROUTES.CHECKOUT} element={<Checkout/>}/>
+        <Route path={ROUTES.ORDER_CONFIRMATION} element={<OrderConfirmation/>}/>
+        <Route path={ROUTES.MENU_MANAGEMENT} element={<MenuitemManagement/>}/>
+        <Route path={ROUTES.ORDER_MANAGEMENT} element={<OrderManagement/>}/>
     </Routes>
     );
 
---
+++ ClientApp/MangofusionReact/src/utility/constants.js
@@ -0,0 +1,10 @@
+export const ROUTES={
+    HOME:'/',
+    CART:'/cart',
+    CHECKOUT:'/checkout',
+    LOGIN:'/login',
+    REGISTER:'/register',
+    ORDER_CONFIRMATION:'/order-confirmation',
+    MENU_MANAGEMENT:'/menu-management',
+    ORDER_MANAGEMENT:'/order-management'
+};
