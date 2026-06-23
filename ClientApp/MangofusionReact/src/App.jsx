4707cb21b974f0b7fbcec222e1335697a7349791
AnuBapatla <anusrichatala@gmail.com>
2026-06-22 22:11:30 -0400

    footer component

--- ClientApp/MangofusionReact/src/App.jsx
+++ ClientApp/MangofusionReact/src/App.jsx
@@ -1,14 +1,18 @@
 import Headers from './components/layout/Header'
 import './App.css'
 import Approutes from './routes/AppRouter'
+import Footer from './components/layout/Footer'
 
 function App() {
   return (
-    <>
+    <div className="d-flex flex-column min-vh-100 bg-body">
       <Headers/>
-      <Approutes/>              
+      <main className="flex-grow-1">
+      <Approutes/>
+      </main>
+      <Footer/>              
       
-    </>
+    </div>
   );
 }
 
---
+++ ClientApp/MangofusionReact/src/components/layout/Footer.jsx
@@ -0,0 +1,12 @@
+function  Footer(){
+    return(
+        <footer className="mt-auto py-3 bg-body-tertiary border-top">
+            <div className="container small text-muted flex-sm-row text-center">
+             <span>crafted with <i className="bi bi-heart-fill text-primary">dotnetmastery</i>
+                </span> 
+                </div>
+        </footer>
+    );
+
+}
+export default Footer;
